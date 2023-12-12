36005fec83ccf9824be65335e5ba61ab83a90500
Timo <dev@2515.ch>
2023-12-12 09:33:27 +0100

    Update to 0.2

--- tests/example_test.rs
+++ tests/example_test.rs
@@ -1,45 +1,97 @@
 #![no_std]
 #![no_main]
 
-use esp32c6_hal as _; // exception handler
-use panic_probe as _; // semihosting::process::abort on test failure
-
 #[cfg(test)]
 #[embedded_test::tests]
 mod unit_tests {
 
-    #[test]
-    #[cfg(feature="log")]
-    fn log() {
+    // import hal which provides exception handler
+    use esp32c6_hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, IO};
+
+    use panic_probe as _; // calls semihosting::process::abort on test failure, printing is done by probe-rs
+
+    // Optional: A init function which is called before every test
+    // asyncness is optional and needs feature embassy
+    #[init]
+    async fn init() -> IO {
+        let peripherals = Peripherals::take();
+        let system = peripherals.SYSTEM.split();
+        ClockControl::boot_defaults(system.clock_control).freeze();
+        let io = IO::new(peripherals.GPIO, peripherals.IO_MUX);
+
+        #[cfg(feature = "log")]
         esp_println::logger::init_logger_from_env();
+
+        // The init function can return some state, which can be consumed by the testcases
+        io
+    }
+
+    // A test which takes the state returned by the init function (optional)
+    // asyncness is optional and needs feature embassy
+    #[test]
+    async fn takes_state(_state: IO) {
+        assert!(true)
+    }
+
+    // Example for a test which is conditionally enabled
+    #[test]
+    #[cfg(feature = "log")]
+    fn log() {
         log::info!("Hello, log!"); // Prints via esp-println to rtt
         assert!(true)
     }
 
+    // Another example for a conditionally enabled test
     #[test]
-    #[cfg(feature="defmt")]
+    #[cfg(feature = "defmt")]
     fn defmt() {
         use defmt_rtt as _;
         defmt::info!("Hello, defmt!"); // Prints via defmt-rtt to rtt
         assert!(true)
     }
 
+    // A test which is cfg'ed out
     #[test]
     #[cfg(abc)]
     fn it_works_disabled() {
         assert!(false)
     }
 
+    // Tests can be ignored with the #[ignore] attribute
     #[test]
     #[ignore]
-    #[cfg(not(abc))]
     fn it_works_ignored() {
         assert!(false)
     }
 
+    // A test that fails with a panic
     #[test]
-    #[cfg(not(abc))]
-    fn it_works() {
+    fn it_fails1() {
         assert!(false)
+    }
+
+    // A test that fails with a returned Err()
+    #[test]
+    fn it_fails2() -> Result<(), ()> {
+        Err(())
+    }
+
+    // Tests can be annotated with #[should_panic] if they are expected to panic
+    #[test]
+    #[should_panic]
+    fn it_passes() {
+        assert!(false)
+    }
+
+    // This test should panic, but doesn't => it fails
+    #[test]
+    #[should_panic]
+    fn it_fails3() {}
+
+    // Tests can be annotated with #[timeout(<secs>)] to change the default timeout of 60s
+    #[test]
+    #[timeout(10)]
+    fn it_timeouts() {
+        loop {} // should run into the 60s timeout
     }
 }
